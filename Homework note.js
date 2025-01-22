029bde32359b7ca2536726f1b6789e1d2494d120
Phil3050 <Pongpansripiboon@gmail.com>
2025-01-22 19:06:10 +0700

    Homework-22-1-25

---
+++ Homework note.js
@@ -0,0 +1,106 @@
+
+// 4.8 let sum = 0; 
+// let count = 0; 
+// let number;
+
+
+// while (true) {
+//     number = parseFloat(prompt('Enter a number : '));
+
+//     if (number < 0) {
+//         break; 
+//     }
+
+//     sum += number; 
+//     count++; 
+
+// if (count > 0) {
+//     let average = sum / count; 
+//     console.log('Average is:', average);
+// } else {
+//     console.log('No positive numbers entered.'); 
+// }
+// }
+
+
+// 4.9 let max = Number.NEGATIVE_INFINITY; 
+// let num;
+
+// while (true) {
+//     num = parseFloat(prompt('Enter a number (stop with 0): ')); 
+
+//     if (num === 0) {
+//         break; 
+//     }
+
+//     if (num > max) {
+//         max = num; 
+//     }
+// }
+
+// if (max > Number.NEGATIVE_INFINITY) {
+//     console.log(`Max number is: ${max}`);
+// } else {
+//     console.log('No numbers were entered.');
+// }
+
+
+
+// 4.10 let continueProgram;
+
+// do {
+
+//     let num1 = parseFloat(prompt("Enter the first number: "));
+//     let num2 = parseFloat(prompt("Enter the second number: "));
+
+//     let operation = prompt('Enter "add" to add or "sub" to sub: ').toLowerCase();
+
+//     if (operation === 'add') {
+//         console.log(`Result: ${num1 + num2}`);
+//     } else if (operation === 'sub') {
+//         console.log(`Result: ${num1 - num2}`);
+//     } else {
+//         console.log('Invalid operation! Please enter "add" or "sub".');
+//     }
+
+//     continueProgram = prompt("Do you want to continue? (y/n): ").toLowerCase();
+// } while (continueProgram === "y");
+
+// 5.1 let prompt = require('prompt-sync')();
+
+// let addNumbers = (num1, num2) => num1 + num2;
+
+// let num1 = parseFloat(prompt('Enter the first number: '));
+// let num2 = parseFloat(prompt('Enter the second number: '));
+
+// console.log(`The sum is: ${addNumbers(num1, num2)}`);
+
+
+// 5.2 let isPositiveNumber = (num) => num > 0;
+
+// console.log(isPositiveNumber(parseFloat(prompt('Enter a number: '))) 
+//     ? "The number is positive." 
+//     : "The number is not positive.");
+
+// 5.3 console.log(prompt('Enter a text: ').toLowerCase());
+
+// 5.4 function isEven(num) {
+//     return num % 2 === 0;
+// }
+
+
+// console.log(isEven(4));  
+// console.log(isEven(7));  
+
+// 5.5 const fToC = f => (f - 32) / 1.8;
+// console.log(fToC(32)); 
+
+// 5.6 const isOdd = n => n % 2 !== 0;
+// console.log(isOdd(7)); 
+
+// 5.7 const difference = (a, b) => Math.abs(a - b);
+// console.log(difference(10, 5)); 
+
+// 5.8 const isPrime = n => n > 1 && [...Array(n).keys()].slice(2).every(i => n % i);
+// console.log(isPrime(7));  
+// console.log(isPrime(0)); 
--- index.js
+++ index.js
@@ -1,3 +1,49 @@
 const prompt = require('prompt-sync')();
-let name = prompt("Enter Your Name : ");
-console.log(name);
+function print(...str){
+    console.log(...str)
+}
+function add(num1,num2) {
+    return num1 + num2
+}
+
+// arrow function
+// const sub = (num1,num2) => num1 - num2;
+// print(sub(10,5));
+// print(add(1,1));
+// print("Okay");
+
+// array & object
+let fruits = ["mango", "apple", "banana"];
+// for (let i = 0; 1 < fruits.length; i++) {
+//     print(fruits[1]);
+// }
+
+// for (const element of fruits) {
+//     print(fruit);
+// }
+
+let objStd = {
+    id: 43169,
+    fname: "Pongpan",
+    fname: "Sripiboon"
+}
+
+print(objStd[1]);
+// for (let i in fruits) {
+//     print(fruits[1]);
+
+// }
+
+// let run = true
+// let char = "";
+// while (run) {
+//  console.log("Hello");
+// }
+
+// //     char = prompt("End Program pass 'q' : ");
+// //     // char[0] === 'q' ? run = false : run = true;
+// //     if (char[0] === 'q') break;
+// //     else continue;
+
+// arrow fuction
+const add = (n1,n2) => n1 + n2
